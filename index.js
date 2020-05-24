5e47a9accfd26a6e3ff2d4b57f5cd8737aee9842
Valeria Meza <valeriameza173@gmail.com>
2020-05-24 14:33:14 -0500

    changed the loop to be for each instead.

--- index.js
+++ index.js
@@ -9,24 +9,13 @@ let seven = document.getElementById("seven");
 let eight = document.getElementById("eight");
 let nine = document.getElementById("nine");
 
-let randomize = [one, two, three, four, five, six, seven, eight, nine]
+// score
 
-randomArray(randomize);
+let boxes = [one, two, three, four, five, six, seven, eight, nine];
+let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
 
-function randomArray(array) {
-  //grab random element of an array and change color.
-  array = randomize[Math.floor(Math.random() * randomize.length)];
-  console.log(array)
-    if(array) {
-        array.style.background = "blue";
-        array.addEventListener("click", hit);
-    }
-}
-
-function hit() {
-    alert ("hit");
-}
-
-
+boxes.forEach(function() {
+    randomBox.style.background = "blue";
+});
 
 
