d0612587666fc51e1af30c7205217efb75511d2a
Valeria Meza <valeriameza173@gmail.com>
2020-05-23 20:03:12 -0500

    added a function to randomise the box colors.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 "use strict";
-console.log("Hello Javascript!");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
 
+let randomize = [one, two, three, four, five, six, seven, eight, nine]
+
+function randomArray() {
+  //grab random element of an array and change color.
+  let randomColor = randomize[Math.floor(Math.random() * randomize.length)];
+  console.log(randomColor)
+    if(randomColor) {
+        randomColor.style.background = "blue";
+    }
+}
+
+randomArray(randomize);
