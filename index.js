3f599e81ebf05a00926597e2747a50ea8d42c063
Valeria Meza <valeriameza173@gmail.com>
2020-05-25 13:57:40 -0500

    feat: cleaning code to make it more easier to read.

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 "use strict";
-let one = document.getElementById("one");
-let two = document.getElementById("two");
-let three = document.getElementById("three");
-let four = document.getElementById("four");
-let five = document.getElementById("five");
-let six = document.getElementById("six");
-let seven = document.getElementById("seven");
-let eight = document.getElementById("eight");
-let nine = document.getElementById("nine");
 
-// score
-
-let boxes = [one, two, three, four, five, six, seven, eight, nine];
-let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
-
-boxes.forEach(function() {
-    randomBox.style.background = "blue";
-});
-
-
+// get the all boxes
+let boxes = document.querySelectorAll("#boxes");
+let time = document.querySelectorAll("#time");
